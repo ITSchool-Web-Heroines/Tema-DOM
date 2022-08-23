1e98efeaae1e98bc1e66e23388aa6fba481aa8d4
NicoA8 <nicoastancai@gmail.com>
2022-08-23 13:11:02 +0300

    Rezolvare

--- src/script.js
+++ src/script.js
@@ -5,3 +5,29 @@ La apăsarea unui buton (.buttons a):
     - Adaugă clasa "active" la butonul apăsat
     - Adaugă clasa "active" la tab-ul identificat prin atributul data-target
 */
+const buttons = document.querySelectorAll(".buttons a");
+const articles = document.querySelectorAll(".tabs article");
+buttons[0].addEventListener("click", () => {
+  buttons[0].classList.add("active");
+  buttons[1].classList.remove("active");
+  buttons[2].classList.remove("active");
+  articles[0].classList.add("active");
+  articles[1].classList.remove("active");
+  articles[2].classList.remove("active");
+});
+buttons[1].addEventListener("click", () => {
+  buttons[0].classList.remove("active");
+  buttons[1].classList.add("active");
+  buttons[2].classList.remove("active");
+  articles[0].classList.remove("active");
+  articles[1].classList.add("active");
+  articles[2].classList.remove("active");
+});
+buttons[2].addEventListener("click", () => {
+  buttons[0].classList.remove("active");
+  buttons[1].classList.remove("active");
+  buttons[2].classList.add("active");
+  articles[0].classList.remove("active");
+  articles[1].classList.remove("active");
+  articles[2].classList.add("active");
+});
